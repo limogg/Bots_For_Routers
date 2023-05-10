95bb0fb0bc5874046485ffa5f43b43ace1a7d078
limogg <limoludek@gmail.com>
2023-05-10 19:32:47 +0200

    2 setup

--- main.py
+++ main.py
@@ -8,16 +8,19 @@ from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
 options.add_experimental_option("detach", True)
-
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get("https://jqueryui.com/resources/demos/progressbar/download.html")
+
+
+driver.get("https://www.saucedemo.com/?ref=hackernoon.com")
 
 #wait for 30s in cant find element it is setup for whole project ;)
 driver.implicitly_wait(30)
 
-element = driver.find_element(By.ID, 'downloadButton')
-element.click()
+user_id = driver.find_element(By.ID, 'user-name')
+user_password = driver.find_element(By.ID, 'password')
+login_button = driver.find_element(By.ID, 'login-button')
 
-progress_element = driver.find_element(By.CLASS_NAME, 'progress-label')
-print(f"{progress_element.text == 'Complete!'}")
+user_id.send_keys('standard_user')
+user_password.send_keys('secret_sauce')
+login_button.click()
