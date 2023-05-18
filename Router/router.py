79a46c841fb46f931efe45703cc202d2d6543297
limogg <limoludek@gmail.com>
2023-05-18 19:10:21 +0200

    Setup interface

--- Router/router.py
+++ Router/router.py
@@ -21,11 +21,10 @@ driver.implicitly_wait(30)
 class Router():
     auto_gui = Auto_Gui()
     #open starting site
-    def open_starting_site(self):
-        driver.get(const.starting_site)
+    def open_starting_site(self, default_ip):
+        default_ip = default_ip
+        driver.get(default_ip)
         self.auto_gui.login_to_site()
-
-        self.switch_to_navbar()
 
 
     #Seek for navbar than change to his frame
@@ -34,9 +33,46 @@ class Router():
         frame = driver.find_element(By.XPATH, '/html/frameset/frame[2]')
         driver.switch_to.frame(frame)
 
+        #switch to main content for all actions
+    def switch_to_main(self):
+        driver.switch_to.default_content()
+        frame = driver.find_element(By.XPATH, '/html/frameset/frame[3]')
+        driver.switch_to.frame(frame)
+
+
 
     #switch to frame and go to interface setup
-    def go_and_set_interface(self):
+    def go_and_set_interface(self, PPoE, password, vlan):
+        ppoe = PPoE
+        password = password
+        vlan = vlan
+
         interface = driver.find_element(By.XPATH, '//*[@id="mainnavibar"]/td[1]/a')
         interface.click()
+
+        self.switch_to_main()
+
+        choose_ppoe = driver.find_element(By.XPATH, '/html/body/form/div/div[2]/table[1]/tbody/tr[4]/td[5]/input')
+        choose_ppoe.click()
+
+        choose_vlan = driver.find_element(By.XPATH, '//*[@id="div_8021q"]/table/tbody/tr[2]/td[5]/input[1]')
+        choose_vlan.click()
+
+        pass_vlan = driver.find_element(By.XPATH, '//*[@id="div_8021q"]/table/tbody/tr[3]/td[5]/input')
+        pass_vlan.send_keys(vlan)
+
+        pass_username = driver.find_element(By.XPATH, '//*[@id="div_isp2"]/table/tbody/tr[2]/td[5]/input')
+        pass_username.send_keys(ppoe)
+
+        pass_pasword = driver.find_element(By.XPATH, '//*[@id="div_isp2"]/table/tbody/tr[4]/td[5]/input')
+        pass_pasword.send_keys(password)
+
+        set_ip_adress = driver.find_element(By.XPATH, '//*[@id="div_ipv4getip"]/table/tbody/tr[2]/td[5]/input[2]')
+        set_ip_adress.click()
+
+        #comented for easier work
+        # save_interface = driver.find_element(By.XPATH, '/html/body/form/div/div[2]/table[3]/tbody/tr/td[3]/input[1]')
+        # save_interface.click()
+
+
 
--- run.py
+++ run.py
@@ -1,10 +1,17 @@
 from Router.router import Router
+import Router.constants as const
+
+PPoE = 'user'
+password = 'password'
+vlan = '100'
+default_ip = const.starting_site
 
 router = Router()
 
-router.open_starting_site()
+router.open_starting_site(default_ip)
 router.switch_to_navbar()
-router.go_and_set_interface()
+#pass everything to setup
+router.go_and_set_interface(PPoE, password, vlan)
 
 
 
